750e0581d9125ee14aa912bcf8511ee36f204a44
hr_khosravi <hamidreza.khosravi@troweb.com>
2025-11-22 18:05:33 +0330

    refactor: Remove unused import from calendar events settings screen
    - Deleted the unused import of Matrix4 from the calendar_events_settings_screen.dart file to clean up the code and improve readability.

--- lib/screens/calendar_events_settings_screen.dart
+++ lib/screens/calendar_events_settings_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:vector_math/vector_math_64.dart' show Matrix4;
 import '../config/app_icons.dart';
 import '../config/theme_colors.dart';
 import '../config/theme_roles.dart';
